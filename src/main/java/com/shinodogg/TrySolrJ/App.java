233cf3163e73054c3be79d31d5431e71586977ff
Eiji Shinohara <shinodogg@gmail.com>
2013-12-10 15:31:46 +0900

    add library settings to pom.xml

--- src/main/java/com/shinodogg/TrySolrJ/App.java
+++ src/main/java/com/shinodogg/TrySolrJ/App.java
@@ -1,5 +1,9 @@
 package com.shinodogg.TrySolrJ;
 
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.core.CoreContainer;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        String url = "http://localhost:8983/solr/collection1";
+        SolrServer server = new HttpSolrServer(url);
+        
+        CoreContainer container = new CoreContainer.Initializer().initialize();
+        
+        
     }
 }
